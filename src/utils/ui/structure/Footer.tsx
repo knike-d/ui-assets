fc72ade765c8daf6e17e7517311420b59df90e08
knike <52563253+knike-d@users.noreply.github.com>
2024-11-17 05:19:10 +0900

    refactor: componentの型を指定 (#2)

--- src/utils/ui/structure/Footer.tsx
+++ src/utils/ui/structure/Footer.tsx
@@ -1,4 +1,6 @@
-export const Footer = () => {
+import type { FC } from "react";
+
+export const Footer: FC = () => {
   return (
     <footer className="mt-auto flex h-24 w-full items-center justify-center border-t">
       <p>ui-assets</p>
--- src/utils/ui/structure/Header.tsx
+++ src/utils/ui/structure/Header.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import type { FC } from "react";
 import Link from "next/link";
 
-export const Header = () => {
+export const Header: FC = () => {
   return (
     <header className="bg-emerald-500">
       <nav className="flex h-12 items-center font-bold text-white">
