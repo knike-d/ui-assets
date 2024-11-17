a28e27cd8de43b474b27c710521bc6429c83e9f5
knike <52563253+knike-d@users.noreply.github.com>
2024-11-18 02:48:53 +0900

    feat: TOPページにサイト説明を記載 (#7)

---
+++ src/page/top/TopPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const TopPage: FC = () => {
+  return (
+    <main className="mx-auto flex w-full max-w-2xl flex-col items-center justify-between p-3">
+      <div className="w-full">これまでに作成したUIを一覧表示するサイトです。</div>
+    </main>
+  );
+};
