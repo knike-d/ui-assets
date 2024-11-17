b09b6bb6165feab4c041a561f77ee4707bfd5d4d
knike-d <knike.develop@gmail.com>
2024-11-17 18:14:21 +0900

    feat: エリア検索フォームページを作成

---
+++ src/page/searchForm/AreaSearchFormPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const AreaSearchFormPage: FC = () => {
+  return (
+    <main className="mx-auto flex w-full max-w-2xl flex-col items-center justify-between p-3">
+      <h1 className="mb-4 mr-auto text-lg font-bold">エリア検索フォーム</h1>
+    </main>
+  );
+};
