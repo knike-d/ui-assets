8173168899e4c3ec8345be7bf04b82e6c0970159
knike <52563253+knike-d@users.noreply.github.com>
2024-12-07 02:42:25 +0900

    feat: TOPにUIリストページへのカードを実装 (#8)

--- src/utils/ui/Icon/RightArrowIcon.tsx
+++ src/utils/ui/Icon/RightArrowIcon.tsx
@@ -1,16 +1,6 @@
 import type { FC, SVGProps } from "react";
 export const RightArrowIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    height="1em"
-    style={{
-      width: 12,
-      height: 12,
-    }}
-    viewBox="0 0 512 512"
-    width="1em"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
+  <svg height="1em" viewBox="0 0 512 512" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M163.916 0 92.084 71.822l184.174 184.174L92.084 440.178 163.916 512l256-256.004z"
       style={{
