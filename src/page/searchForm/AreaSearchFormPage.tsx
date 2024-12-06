c44095322c719a2fb1ca14fa4ce1971749241455
knike-d <knike.develop@gmail.com>
2024-12-07 02:46:15 +0900

    refactor: パーツページのレイアウトをコンポーネントに切り出す

--- src/page/searchForm/AreaSearchFormPage.tsx
+++ src/page/searchForm/AreaSearchFormPage.tsx
@@ -1,11 +1,11 @@
 import type { FC } from "react";
 import { AreaSearchForm } from "@/features/searchForm/AreaSearchForm";
+import { UIAssetPartsPageLayout } from "@/features/uiAsset/UIAssetPartsPageLayout";
 
 export const AreaSearchFormPage: FC = () => {
   return (
-    <main className="mx-auto flex w-full max-w-2xl flex-col items-center justify-between p-3">
-      <h1 className="mb-4 mr-auto text-lg font-bold">エリア検索フォーム</h1>
+    <UIAssetPartsPageLayout title="アコーディオン">
       <AreaSearchForm />
-    </main>
+    </UIAssetPartsPageLayout>
   );
 };
