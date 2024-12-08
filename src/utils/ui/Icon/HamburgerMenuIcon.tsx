5bc26128fda63b4dcf1c6e6eb9b34a0eb1f92f54
knike-d <knike.develop@gmail.com>
2024-12-09 01:30:21 +0900

    refactor: ハンバーガーボタンの色を親から変えられるようにする

--- src/utils/ui/Icon/HamburgerMenuIcon.tsx
+++ src/utils/ui/Icon/HamburgerMenuIcon.tsx
@@ -1,22 +1,13 @@
 import type { FC, SVGProps } from "react";
 export const HamburgerMenuIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
   <svg
-    height="1em"
-    style={{
-      width: 20,
-      height: 20,
-      opacity: 1,
-    }}
+    className="fill-white"
+    height="20px"
     viewBox="0 0 512 512"
-    width="1em"
+    width="20px"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      d="M0 16h512v80H0zM0 216.008h512v79.984H0zM0 416h512v80H0z"
-      style={{
-        fill: "#fff",
-      }}
-    />
+    <path d="M0 16h512v80H0zM0 216.008h512v79.984H0zM0 416h512v80H0z" />
   </svg>
 );
