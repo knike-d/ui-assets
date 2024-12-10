f7994e7c28b29b7d7996363a7a54f0d5cadd641f
knike-d <knike.develop@gmail.com>
2024-12-11 01:08:16 +0900

    feat: メッセージページの外枠作成

---
+++ src/page/message/MessageDetailPage.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react";
+
+type Props = {
+  storeId: string;
+};
+
+export const MessageDetailPage: FC<Props> = ({ storeId }) => {
+  return <main className="flex max-h-dvh flex-col" />;
+};
