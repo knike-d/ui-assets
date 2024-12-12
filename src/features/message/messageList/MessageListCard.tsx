c2dde9e35f06b76e6c256043e3905e784e8db424
knike-d <knike.develop@gmail.com>
2024-12-13 03:32:02 +0900

    fix: メッセージページへの遷移をソフトナビゲーションに変更

--- src/features/message/messageList/MessageListCard.tsx
+++ src/features/message/messageList/MessageListCard.tsx
@@ -1,4 +1,5 @@
 import type { FC, ReactNode } from "react";
+import Link from "next/link";
 
 type Props = {
   href: string;
@@ -7,8 +8,8 @@ type Props = {
 
 export const MessageListCard: FC<Props> = ({ href, children }) => {
   return (
-    <a className="flex w-full items-center border-b p-8 hover:bg-slate-200" href={href}>
+    <Link className="flex w-full items-center border-b p-8 hover:bg-slate-200" href={href}>
       <h3 className="text-lg">{children}</h3>
-    </a>
+    </Link>
   );
 };
