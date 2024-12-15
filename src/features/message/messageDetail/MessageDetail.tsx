6bc5b57c7065e35897c536049dfb1ca028615353
knike-d <knike.develop@gmail.com>
2024-12-16 00:10:11 +0900

    feat: メッセージ詳細を作成

---
+++ src/features/message/messageDetail/MessageDetail.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useCallback, useLayoutEffect, useRef, type FC } from "react";
+import { MESSAGE_DETAIL_FETCH_LIMIT, useFetchMessageDetail } from "@/features/message/useFetchMessageDetail";
+import { useInViewTrigger } from "@/utils/hooks/interaction/useInViewTrigger";
+import { Spinner } from "@/utils/ui/loading/Spinner";
+
+type Props = {
+  storeId: string;
+};
+
+export const MessageDetail: FC<Props> = ({ storeId }) => {
+  const { data: messageDetail, isValidating, isLoading, setSize } = useFetchMessageDetail(storeId);
+  const messages = messageDetail?.map((el) => el.slice()).flat() || [];
+  const isLastData = !!messageDetail && (messageDetail.slice(-1)[0]?.length || 0) < MESSAGE_DETAIL_FETCH_LIMIT;
+
+  const handleInView = useCallback(() => {
+    setSize((size) => size + 1);
+  }, [setSize]);
+  const triggerElement = useInViewTrigger(handleInView, {
+    skip: isValidating || isLastData,
+  });
+
+  const messageDetailRef = useRef<HTMLUListElement>(null);
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      messageDetailRef.current?.scrollIntoView(false);
+    }
+  }, [isLoading]);
+
+  return (
+    <ul
+      ref={messageDetailRef}
+      className="flex w-full max-w-2xl flex-1 flex-col-reverse overflow-auto border p-2 shadow-md"
+    >
+      {messages.map((el) => (
+        <li key={el.id}>{el.content}</li>
+      ))}
+      <li className="m-3 mb-auto grid place-items-center">{isValidating ? <Spinner /> : triggerElement}</li>
+    </ul>
+  );
+};
--- src/page/message/MessageDetailPage.tsx
+++ src/page/message/MessageDetailPage.tsx
@@ -1,4 +1,5 @@
 import { type FC } from "react";
+import { MessageDetail } from "@/features/message/messageDetail/MessageDetail";
 import { MessageDetailPageHeader } from "@/features/message/messageDetail/MessageDetailPageHeader";
 
 type Props = {
@@ -9,6 +10,7 @@ export const MessageDetailPage: FC<Props> = ({ storeId }) => {
   return (
     <main className="flex h-dvh max-h-dvh flex-col items-center">
       <MessageDetailPageHeader storeId={storeId} />
+      <MessageDetail storeId={storeId} />
     </main>
   );
 };
