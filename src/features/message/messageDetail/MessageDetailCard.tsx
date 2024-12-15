2c9ea39f3dedc704a667e33a2e538eb46013809f
knike-d <knike.develop@gmail.com>
2024-12-16 03:32:42 +0900

    feat: メッセージカードcomponentを作成

---
+++ src/features/message/messageDetail/MessageDetailCard.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+import { type FC } from "react";
+
+type Props = {
+  fromStore: boolean;
+  children: ReactNode;
+};
+
+export const MessageDetailCard: FC<Props> = ({ fromStore, children }) => {
+  return (
+    <p
+      className={`break-words rounded-lg p-4 text-sm  ${fromStore ? "rounded-ss-none bg-gray-200 text-black" : "rounded-se-none bg-blue-600 text-white"}`}
+    >
+      {children}
+    </p>
+  );
+};
