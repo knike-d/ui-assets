87c6476a5672281429a3cef6d02ccab259099353
knike-d <knike.develop@gmail.com>
2024-12-17 15:10:49 +0900

    chore: テスト環境構築

---
+++ src/testing/setup-tests.ts
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom/vitest";
+
+import { initializeDb, resetDb } from "@/testing/mocks/db";
+import { server } from "@/testing/mocks/server";
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+afterAll(() => server.close());
+beforeEach(() => {
+  initializeDb();
+});
+afterEach(() => {
+  server.resetHandlers();
+  resetDb();
+});
