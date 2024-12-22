9861a0d87813a18ca71c2e2874543b733d63a792
knike-d <knike.develop@gmail.com>
2024-12-23 01:13:31 +0900

    fix: ナビゲーションをハードからソフトに変更

--- src/features/uiAsset/UIAssetCard.tsx
+++ src/features/uiAsset/UIAssetCard.tsx
@@ -1,4 +1,5 @@
 import type { FC, ReactNode } from "react";
+import Link from "next/link";
 import { RightArrowIcon } from "@/utils/ui/Icon/RightArrowIcon";
 
 type Props = {
@@ -8,12 +9,12 @@ type Props = {
 
 export const UIAssetCard: FC<Props> = ({ href, children }) => {
   return (
-    <a
+    <Link
       className="flex h-16 w-11/12 items-center justify-between rounded-md border p-4 text-lg hover:bg-slate-100"
       href={href}
     >
       {children}
       <RightArrowIcon />
-    </a>
+    </Link>
   );
 };
