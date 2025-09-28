f193f7845e00582308ba996698c959bc8ded1aef
knike-d <knike.develop@gmail.com>
2025-09-29 00:26:01 +0900

    refactor: 名前空間を経由せずにReactNodeを使用する

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "@/app/globals.css";
+import type { ReactNode } from "react";
 import { SWRProvider } from "@/utils/libs/swr/SWRProvider";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <body className="mx-auto flex min-h-dvh w-auto flex-col">
--- src/app/parts/layout.tsx
+++ src/app/parts/layout.tsx
@@ -1,7 +1,8 @@
+import type { ReactNode } from "react";
 import { Footer } from "@/utils/ui/structure/Footer";
 import { Header } from "@/utils/ui/structure/Header";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
